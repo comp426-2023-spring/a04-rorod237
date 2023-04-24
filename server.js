4fb9965cac5084f2742173a1a4f1a8ab0aa6a4d6
Rohan Kumar <rorod237@email.unc.edu>
2023-04-24 19:01:14 -0400

    second

--- server.js
+++ server.js
@@ -1,87 +1,68 @@
 #!/usr/bin/env node
-
-import {rps, rpsls} from "./lib/rpsls.js"
-import minimist from 'minimist'
-import express from 'express'
-
-var argv = minimist(process.argv.slice(2));
-const port = argv.port || 5000;
-
+import {rps, rpsls} from "./lib/rpsls.js";
+import express from 'express';
+import minimist from 'minimist';
 const app = express();
-app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
-app.get('/app', (req, res) => {
-    res.status(200).send('200 OK').end();
-});
-
-/**
- * Random rps shot
- */
-app.get('/app/rps', (req, res) => {
-    res.status(200).send(JSON.stringify(rps(req.body.shot))).end();
-})
-
-/**
- * Random rpsls shot
- */
-app.get('/app/rpsls', (req, res) => {
-    res.status(200).send(JSON.stringify(rpsls(req.body.shot))).end();
-})
-
-/**
- * URLEncoded
- * Note: This is a GET
- * Example: http://localhost:5000/app/rps/play?shot=rock
- */
-app.get('/app/rps/play', (req, res) => {
-    res.status(200).send(JSON.stringify(rps(req.query.shot))).end();
-})
-
-/**
- * URLEncoded
- * Note: This is a GET
- * Example: http://localhost:5000/app/rps/play?shot=spock
- */
-app.get('/app/rpsls/play', (req, res) => {
-    res.status(200).send(JSON.stringify(rpsls(req.query.shot))).end();
-})
-
-/**
- * JSON
- * Note: This is a POST
- */
-app.post('/app/rps/play', (req, res) => {
-    res.status(200).send(JSON.stringify(rps(req.body.shot))).end();
-})
-
-/**
- * JSON
- * Note: This is a POST
- */
-app.post('/app/rpsls/play', (req, res) => {
-    res.status(200).send(JSON.stringify(rpsls(req.body.shot))).end();
-})
-
-/**
- * /:shot syntax allows for that field to be parsed
- */
-app.get('/app/rps/play/:shot', (req, res) => {
-    res.status(200).send(JSON.stringify(rps(req.params.shot))).end();
-})
-
-/**
- * /:shot syntax allows for that field to be parsed
- */
-app.get('/app/rpsls/play/:shot', (req, res) => {
-    res.status(200).send(JSON.stringify(rpsls(req.params.shot))).end();
-})
-
-/**
- * Default (non-existent) endpoint
- */
-app.all('*', (req, res) => {
-    res.status(404).send('404 NOT FOUND').end();
-})
-
+app.use(express.json());
+const args = minimist(process.argv.slice(2)); 
+const port = args["port"] || 5000;
 app.listen(port);
+
+//ALL endpoints should return HTTP headers including a status code and the appropriate content type for the response.
+
+//Check endpoint at /app/ that returns 200 OK.
+app.get('/app/', (req, res) => {
+	res.status(200).send("200 OK");
+})
+
+//Endpoint /app/rps/ that returns {"player":"(rock|paper|scissors)"}. (HINT: regex)
+app.get('/app/rps/', (req, res) => {
+    res.status(200).send(rps());
+})
+
+//Endpoint /app/rpsls/ that returns {"player":"(rock|paper|scissors|lizard|spock)"}
+app.get('/app/rpsls/', (req, res) => {
+    res.status(200).send(rpsls());
+})
+
+//Endpoint /app/rps/play/ should accept request bodies in the following forms: shot=(rock|paper|scissors) (URLEncoded) or {"shot":"(rock|paper|scissors)"} (JSON) as data bodies and return {"player":"(rock|paper|scissors)","opponent":"(rock|paper|scissors)","result":"(win|lose|tie)"}
+app.get('/app/rps/play/', (req, res) => {
+    res.status(200).send(rps(req.query.shot));
+})
+
+app.post('/app/rps/play/', (req, res) => {
+    res.status(200).send(rps(req.body.shot));
+})
+
+//Endpoint /app/rpsls/play/ should accept request bodies in the following forms: shot=(rock|paper|scissors) (URLEncoded) or {"shot":"(rock|paper|scissors)"} (JSON) and return {"player":"(rock|paper|scissors)","opponent":"(rock|paper|scissors)","result":"(win|lose|tie)"}
+app.get('/app/rpsls/play/', (req, res) => {
+    res.status(200).send(rpsls(req.query.shot));
+})
+
+app.post('/app/rpsls/play/', (req, res) => {
+    res.status(200).send(rpsls(req.body.shot));
+})
+
+//Endpoint /app/rpsls/play/(rock|paper|scissors)/ should return {"player":"(rock|paper|scissors)","opponent":"(rock|paper|scissors)","result":"(win|lose|tie)"}
+app.get('/app/rps/play/:shot/', (req, res) => {
+    res.status(200).send(rps(req.params.shot));
+})
+
+// app.post('/app/rps/play/:shot', (req, res) => {
+//     res.status(200).send(rps(req.body.shot));
+// })
+
+//Endpoint /app/rpsls/play/(rock|paper|scissors|lizard|spock)/ should return {"player":"(rock|paper|scissors|lizard|spock)","opponent":"(rock|paper|scissors|lizard|spock)","result":"(win|lose|tie)"}
+app.get('/app/rpsls/play/:shot/', (req, res) => {
+    res.status(200).send(rpsls(req.params.shot));
+})
+
+// app.post('/app/rpsls/play/:shot', (req, res) => {
+//     res.status(200).send(rpsls(req.body.shot));
+// })
+
+//Default API endpoint that returns 404 NOT FOUND for any endpoints that are not defined
+app.get("*", (req, res) => {
+    res.status(404).send("404 NOT FOUND");
+})
